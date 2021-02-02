7e4b4edab47682ef60c2e6c57ee6662e8eddb476
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-02-01 22:20:35 -0700

    Removed index.html and cleaned up code

--- display_results.php
+++ display_results.php
@@ -20,18 +20,6 @@
     else if ($age < 0 ) {
         $error_message = 'Age must be at least 0.'; 
     }
-    /*
-    else if ( $interest_rate > 15 ) {
-        $error_message = 'Interest rate must be less than or equal to 15.';
-    // validate years
-    } else if ( $years === FALSE ) {
-        $error_message = 'Years must be a valid whole number.';
-    } else if ( $years <= 0 ) {
-        $error_message = 'Years must be greater than zero.';
-    } else if ( $years > 30 ) {
-        $error_message = 'Years must be less than 31.';
-    }
-        */
     // set error message to empty string if no invalid entries
     else {
         $error_message = ''; }
